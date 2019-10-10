fa21d66de067d6281e10b2bb51744e76c15589fa
郭云喜 <guoyunxi@ut.cn>
2019-10-10 23:09:45 +0800

    添加RPC demo

---
+++ RPCCall/RPCClient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RPCCall
+{
+    public class RPCClient
+    {
+        private IConnection _connection;
+
+        public RPCClient()
+        {
+            var factory = new ConnectionFactory() {HostName = "localhost"};
+            _connection = factory.CreateConnection();
+            var channel = _connection.CreateModel();
+            var replyQueueName = channel.QueueDeclare().QueueName;
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (o, e) => { };
+
+        }
+
+        public string Call(string str)
+        {
+            return "";
+        }
+
+        public void Close()
+        {
+
+        }
+
+    }
+}
